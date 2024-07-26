516af0a47ea58d817935a2f519d37990ec4f21d0
Artjom Kurapov <artjom.kurapov@clarifai.com>
2024-07-26 20:04:25 +0300

    update blinking and target endpoint payload format

--- README.md
+++ README.md
@@ -3,5 +3,10 @@ A collection of code for IoT sensors for monitoring beehive on the edge devices,
 
 ```mermaid
 flowchart LR
-	hardware-beehive-sensors[<a href="https://github.com/Gratheon/hardware-beehive-sensors">hardware-beehive-sensors</a>] -."send aggregate (5sec)\n metric value".-> telemetry-api[<a href="https://github.com/Gratheon/telemetry-api">telemetry-api</a>]
+	hardware-beehive-sensors[<a href="https://github.com/Gratheon/hardware-beehive-sensors">hardware-beehive-sensors</a>] -."send temperature \n(every 1 min)".-> telemetry-api[<a href="https://github.com/Gratheon/telemetry-api">telemetry-api</a>]
 ```
+
+## Installation
+- Install Dallas Temperature library in Arduino IDE
+  DallasTemperature@3.9.0
+  https://github.com/milesburton/Arduino-Temperature-Control-Library
--- esp32-temperature/esp32-temperature.ino
+++ esp32-temperature/esp32-temperature.ino
@@ -3,67 +3,89 @@
 #include <DallasTemperature.h>
 #include <HTTPClient.h>
 
-// Replace with your network credentials
-const char* ssid     = "your_SSID";
-const char* password = "your_PASSWORD";
 
-// Constants for endpoint
-const char* hiveId = "123";
-const char* apiToken = "456";
+// WIFI access
+#define WIFI_SSID ""
+#define WIFI_PW ""
+
+// Target
 const char* serverName = "https://telemetry.gratheon.com/metric";
+const char* hiveId = ""; // fill in your hive id from https://app.gratheon.com/hives
+const char* apiToken = ""; // fill in your api token from https://app.gratheon.com/account
 
-// Define pin for DS18B20 data line
-#define ONE_WIRE_BUS 4
+
+#define ONE_WIRE_BUS 4 // Define pin for temperature sensor DS18B20 data line
+#define LED_PIN 2
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
-void setup() {
-  Serial.begin(115200);
-  
-  // Connect to Wi-Fi
-  WiFi.begin(ssid, password);
-  Serial.print("Connecting to WiFi...");
-  while (WiFi.status() != WL_CONNECTED) {
+void connectToWiFi() {
+  WiFi.begin(WIFI_SSID, WIFI_PW);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) { 
     delay(500);
     Serial.print(".");
   }
-  Serial.println("Connected!");
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP: ");
+  Serial.println(WiFi.localIP());
+}
 
+
+
+void setup() {
+  Serial.begin(115200);
+  delay(100);
+
+  connectToWiFi();
   sensors.begin();
+
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW); // Ensure LED is off initially
 }
 
 void loop() {
+  // Blink LED
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("WiFi Disconnected");
+    delay(10000); // retry connection every 10 sec
+
+    return;
+  }
+
+  digitalWrite(LED_PIN, HIGH); // Turn LED on
+
   sensors.requestTemperatures();
   float temperatureC = sensors.getTempCByIndex(0);
 
-  if (WiFi.status() == WL_CONNECTED) {
-    HTTPClient http;
+  HTTPClient http;
 
-    // Create the full URL with hiveId and apiToken
-    String url = String(serverName) + "/" + hiveId + "?api_token=" + apiToken;
+  // Create the full URL with hiveId and apiToken
+  String url = String(serverName) + "/" + hiveId + "?api_token=" + apiToken;
 
-    http.begin(url); // Specify the URL
-    http.addHeader("Content-Type", "application/json"); // Specify content-type header
+  http.begin(url); // Specify the URL
+  http.addHeader("Content-Type", "application/json"); // Specify content-type header
 
-    // Create JSON object to send
-    String jsonPayload = "{\"temperature\":" + String(temperatureC) + "}";
+  // Create JSON object to send
+  String jsonPayload = "{\"fields\":{\"temperature\":" + String(temperatureC) + "}}";
+  Serial.print(jsonPayload);
 
-    int httpResponseCode = http.POST(jsonPayload);
+  int httpResponseCode = http.POST(jsonPayload);
 
-    if (httpResponseCode > 0) {
-      String response = http.getString();
-      Serial.println(httpResponseCode);
-      Serial.println(response);
-    } else {
-      Serial.print("Error on sending POST: ");
-      Serial.println(httpResponseCode);
-    }
-
-    http.end(); // Free resources
+  if (httpResponseCode > 0) {
+    String response = http.getString();
+    Serial.println(httpResponseCode);
+    Serial.println(response);
   } else {
-    Serial.println("WiFi Disconnected");
+    Serial.print("Error on sending POST: ");
+    Serial.println(httpResponseCode);
   }
-  
-  delay(10000); // Post data every 10 seconds
+
+  http.end(); // Free resources
+
+  delay(500); // Adjust the delay for desired blink duration
+  digitalWrite(LED_PIN, LOW); // Turn LED off
+
+  delay(60000); // wait for a minute
 }
